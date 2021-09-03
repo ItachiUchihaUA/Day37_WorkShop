72e8520b7fc4cca6ed889990fa911b594a4c473f
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-09-03 11:29:26 +0530

    Ability to add multiple contacts

--- src/main/java/workshop_AddressBook/Ability.java
+++ src/main/java/workshop_AddressBook/Ability.java
@@ -3,10 +3,10 @@ package workshop_AddressBook;
 import java.util.Scanner;
 
 public class Ability {
-	
+
 	protected static void addContact() {
-		int flag = 0;
-		while (flag == 0) {
+		int flag = 1;
+		while (flag == 1) {
 			Contact c = new Contact();
 			Scanner sc = new Scanner(System.in);
 			System.out.println("\n---New Contact---");
@@ -28,60 +28,78 @@ public class Ability {
 			c.setEmail(sc.next());
 			AddressBook.addressBook.add(c);
 			System.out.println("Want to Add more?\n");
-			flag=2;
-			while(flag !=1 || flag != 0 ) {
-				System.out.println("\n only Enter 0 for Yes or 1 for No: ");
-				flag=sc.nextInt();
+			flag = 2;
+			while (flag != 1 || flag != 0) {
+				System.out.println("\n only Enter 1 for Yes or 0 for No: ");
+				flag = sc.nextInt();
 			}
 		}
 	}
-	
+
 	protected static void editContact() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter First Name to Edit : ");
-		String name = sc.next();
-		int flag2 = 0;
-		for (int i = 0; i < AddressBook.addressBook.size(); i++) {
-			if (AddressBook.addressBook.get(i).getFirstName() == name) {
-				flag2 = 1;
-				System.out.println("---Edit Contact---");
-				System.out.println("Enter First Name: ");
-				AddressBook.addressBook.get(i).setFirstName(sc.next());
-				System.out.println("Enter Last Name: ");
-				AddressBook.addressBook.get(i).setLastName(sc.next());
-				System.out.println("Enter Address: ");
-				AddressBook.addressBook.get(i).setAddress(sc.next());
-				System.out.println("Enter City: ");
-				AddressBook.addressBook.get(i).setCity(sc.next());
-				System.out.println("Enter State: ");
-				AddressBook.addressBook.get(i).setState(sc.next());
-				System.out.println("Enter Zip code: ");
-				AddressBook.addressBook.get(i).setZip(sc.next());
-				System.out.println("Enter Phone: ");
-				AddressBook.addressBook.get(i).setPhone(sc.next());
+		int flag = 1;
+		while (flag == 1) {
+			System.out.println("Enter First Name to Edit : ");
+			String name = sc.next();
+			int flag2 = 0;
+			for (int i = 0; i < AddressBook.addressBook.size(); i++) {
+				if (AddressBook.addressBook.get(i).getFirstName() == name) {
+					flag2 = 1;
+					System.out.println("---Edit Contact---");
+					System.out.println("Enter First Name: ");
+					AddressBook.addressBook.get(i).setFirstName(sc.next());
+					System.out.println("Enter Last Name: ");
+					AddressBook.addressBook.get(i).setLastName(sc.next());
+					System.out.println("Enter Address: ");
+					AddressBook.addressBook.get(i).setAddress(sc.next());
+					System.out.println("Enter City: ");
+					AddressBook.addressBook.get(i).setCity(sc.next());
+					System.out.println("Enter State: ");
+					AddressBook.addressBook.get(i).setState(sc.next());
+					System.out.println("Enter Zip code: ");
+					AddressBook.addressBook.get(i).setZip(sc.next());
+					System.out.println("Enter Phone: ");
+					AddressBook.addressBook.get(i).setPhone(sc.next());
 
+				}
+			}
+			if (flag2 == 0) {
+				System.out.println("No Such Contact Found!");
+			}
+			flag = 2;
+			System.out.println("Edit more?");
+			while (flag != 0 || flag != 1) {
+				System.out.println("Only Enter 1 for yes or 0 for No: ");
+				flag = sc.nextInt();
 			}
 		}
-		if (flag2 == 0) {
-			System.out.println("No Such Contact Found!");
-		}
+
 	}
-	
-	static protected void deleteContact() {
+
+	protected static void deleteContact() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter First Name to delete : ");
-		String name = sc.next();
-		int flag2 = 0;
-		for (int i = 0; i < AddressBook.addressBook.size(); i++) {
-			if (AddressBook.addressBook.get(i).getFirstName().equals(name)) {
-				flag2 = 1;
-				AddressBook.addressBook.remove(i);
-				i--;
-				System.out.println("Deleted!");
+		int flag = 1;
+		while (flag == 1) {
+			System.out.println("Enter First Name to delete : ");
+			String name = sc.next();
+			int flag2 = 0;
+			for (int i = 0; i < AddressBook.addressBook.size(); i++) {
+				if (AddressBook.addressBook.get(i).getFirstName().equals(name)) {
+					flag2 = 1;
+					AddressBook.addressBook.remove(i);
+					i--;
+					System.out.println("Deleted!");
+				}
 			}
-		}
-		if (flag2 == 0) {
-			System.out.println("No Such Contact Found!");
+			if (flag2 == 0) {
+				System.out.println("No Such Contact Found!");
+			}
+			flag = 2;
+			while (flag != 0 || flag != 1) {
+				System.out.println("Only Enter 1 for yes or 0 for No: ");
+				flag = sc.nextInt();
+			}
 		}
 	}
 }
--- src/main/java/workshop_AddressBook/AddressBook.java
+++ src/main/java/workshop_AddressBook/AddressBook.java
@@ -10,6 +10,7 @@ public class AddressBook {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book System!");
 		Contact contact = new Contact();
+		
 		Ability.addContact();
 		Ability.editContact();
 		Ability.deleteContact();
