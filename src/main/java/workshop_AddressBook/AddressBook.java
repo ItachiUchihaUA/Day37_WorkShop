9ec4c31bb71d22c8c73e0216a0fefc7a7603b834
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-09-03 09:11:40 +0530

    Created POJO class and an object

--- src/main/java/workshop_AddressBook/AddressBook.java
+++ src/main/java/workshop_AddressBook/AddressBook.java
@@ -4,6 +4,6 @@ public class AddressBook {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Address Book System!");
-
+		Contact contact = new Contact();
 	}
 }
--- src/main/java/workshop_AddressBook/Contact.java
+++ src/main/java/workshop_AddressBook/Contact.java
@@ -1,5 +1,69 @@
 package workshop_AddressBook;
 
 public class Contact {
-
+	private String firstName;
+	private String lastName;
+	private String Address;
+	private String city;
+	private String state;
+	private String email;
+	private String Phone;
+	private String zip;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public String getZip() {
+		return zip;
+	}
+	public void setZip(String zip) {
+		this.zip = zip;
+	} 
+	
+	public boolean firstEquals(String c) {
+		return this.getFirstName().equals(c);
+	}
+	
+	public boolean cityEquals(String c) {
+		return this.getCity().equals(c);
+	}
 }
