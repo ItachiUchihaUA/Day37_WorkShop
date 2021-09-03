42fb222006317aafe1615322d863af96027aba4f
Ujjwal Aakash <ujjwalaakash46ua@gmail.com>
2021-09-03 20:06:48 +0530

    Adding to database

--- src/main/java/workshop_AddressBook/DataBase.java
+++ src/main/java/workshop_AddressBook/DataBase.java
@@ -2,20 +2,40 @@ package workshop_AddressBook;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class DataBase {
-	
+
 	protected static Connection newC;
-	
+
 	protected static void connectionToDatabase(String URL) {
 		try {
-		newC = DriverManager.getConnection(URL, "ujjwal", "Ujjwal123");
-		if(newC!=null) {
-			System.out.println("Connected to Database");
-		}
-		}catch(Exception e) {
+			newC = DriverManager.getConnection(URL, "ujjwal", "Ujjwal123");
+			if (newC != null) {
+				System.out.println("Connected to Database");
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
+
+	protected static void insertSQL(String table, Contact c) {
+
+		try {
+			PreparedStatement p = newC.prepareStatement("insert into " + table + " values(?,?,?,?,?,?,?,?)");
+			p.setString(1, c.getFirstName());
+			p.setString(2, c.getLastName());
+			p.setString(3, c.getAddress());
+			p.setString(4, c.getCity());
+			p.setString(5, c.getState());
+			p.setString(6, c.getZip());
+			p.setString(7, c.getPhone());
+			p.setString(8, c.getPhone());
+			p.execute();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
 }
